f96cab36b160ae458fdacb714fbf5f21e6bdaa2f
Payman Salek <psalek@miu.edu>
2020-06-23 15:34:23 -0500

    Initial commit

---
+++ src/test/java/edu/miu/cs/cs544/examples/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs544.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
